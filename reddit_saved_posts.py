b245e746d1ce67ffbbac1cc6de82bd0508aee0c8
vihanggodbole <vihang16091996@gmail.com>
2016-12-11 12:13:30 +0530

    add search functionality

--- reddit_saved_posts.py
+++ reddit_saved_posts.py
@@ -1,26 +1,24 @@
 import praw
 from pprint import pprint
-
+from getpass import getpass
 
 def login():
     '''logs in the user using OAuth 2.0 and returns a redditor object for use'''
-    user_agent = 'osx:reddit_saved_posts_search: v0.1 (by /u/idreamapple)'
-    # app_scopes = 'account creddits edit flair history identity livemanage
-    # modconfig modcontributors modflair modlog modothers modposts modself
-    # modwiki mysubreddits privatemessages read report save submit subscribe
-    # vote wikiedit wikiread'
-    r = praw.Reddit('mysettings', user_agent=user_agent)
-    return r
+    username = input('Username: ')
+    password = getpass(prompt='Password: ')
+    user_agent = 'osx:reddit_saved_posts_search: v0.1 (by /u/{})'.format(username)
+    r = praw.Reddit('mysettings', user_agent=user_agent, username=username, password=password)
+    return r.user.me()
 
 
 def show_saved_post_by_subreddit(saved_posts):
-    local_subreddits = []
+    subreddit_names = []
     for post in saved_posts:
-        local_subreddits.append(post.subreddit.display_name)
+        subreddit_names.append(post.subreddit.display_name)
 
     subreddits = {}
     print('Number of saved posts by subreddit -')
-    for subreddit in local_subreddits:
+    for subreddit in subreddit_names:
         if subreddit not in subreddits:
             subreddits[subreddit] = 1
         else:
@@ -29,29 +27,37 @@ def show_saved_post_by_subreddit(saved_posts):
     pprint(subreddits)
 
 
+def search(text, saved_posts):
+    i = 1
+    for title in saved_posts:
+        if text in title:
+            print('\n{}. {}'.format(i, title))
+            i += 1
+
+
 def main():
-    reddit_obj = login()
-    authenticated_user = reddit_obj.get_me()
-    print('Welcome /u/{}. We have been expecting you.'.format(authenticated_user.name))
-    saved = authenticated_user.get_saved(sort='new', time='all', limit=None)
+    redditor = login()
+    print('Welcome /u/{}. We have been expecting you.'.format(redditor))
+    saved = redditor.saved(limit=None)
 
     saved_posts = []
     saved_comments = []
 
-    for post in saved:  # separate posts and comments
-        if isinstance(post, praw.objects.Submission):
+    for post in saved:  # separate out posts and comments
+        if isinstance(post, praw.models.Submission):
             saved_posts.append(post)
-        elif isinstance(post, praw.objects.Comment):
+        elif isinstance(post, praw.models.Comment):
             saved_comments.append(post)
 
-    saved_posts_title = set()
+    saved_posts_title = []
 
-    for post in saved_posts:    # create a set of saved posts titles
-        saved_posts_title.add(post.title)
-
-    # print('\n\n\n\nSaved comments - ')
-    # for comment in saved_comments:
-    #    pprint(comment.body)
+    for post in saved_posts:    # create a list of saved posts' titles
+        saved_posts_title.append(post.title)
+        # print(post.title)
+    while(True):
+        text_to_search = input('\nEnter the text to search:')
+        search(text=text_to_search, saved_posts=saved_posts_title)
+        url = print('\n[Post Number] to open the link in your default web browser')
 
 
 if __name__ == '__main__':
