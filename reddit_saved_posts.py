ddf49dca2600744136f1ce873ac6b39e11e0f197
vihanggodbole <vihang16091996@gmail.com>
2016-12-15 15:55:53 +0530

    add link,self post, comments separation

--- reddit_saved_posts.py
+++ reddit_saved_posts.py
@@ -1,38 +1,33 @@
 import praw
+import webbrowser
 from pprint import pprint
 from getpass import getpass
+
 
 def login():
     '''logs in the user using OAuth 2.0 and returns a redditor object for use'''
     username = input('Username: ')
     password = getpass(prompt='Password: ')
-    user_agent = 'osx:reddit_saved_posts_search: v0.1 (by /u/{})'.format(username)
-    r = praw.Reddit('mysettings', user_agent=user_agent, username=username, password=password)
+    user_agent = 'reddit_saved_posts_search: v0.2 (by /u/{})'.format(
+        username)
+    r = praw.Reddit('mysettings', user_agent=user_agent,
+                    username=username, password=password)
     return r.user.me()
-
-
-def show_saved_post_by_subreddit(saved_posts):
-    subreddit_names = []
-    for post in saved_posts:
-        subreddit_names.append(post.subreddit.display_name)
-
-    subreddits = {}
-    print('Number of saved posts by subreddit -')
-    for subreddit in subreddit_names:
-        if subreddit not in subreddits:
-            subreddits[subreddit] = 1
-        else:
-            subreddits[subreddit] += 1
-
-    pprint(subreddits)
 
 
 def search(text, saved_posts):
     i = 1
-    for title in saved_posts:
-        if text in title:
-            print('\n{}. {}'.format(i, title))
+    searched_posts = {}
+    for post_id in saved_posts:
+        if text.lower() in saved_posts[post_id].lower():
+            print('\n{}. {}'.format(i, saved_posts[post_id]))
+            searched_posts[i] = post_id
             i += 1
+    return searched_posts
+
+
+def open_in_browser(url):
+    webbrowser.open_new_tab(url)
 
 
 def main():
@@ -42,22 +37,82 @@ def main():
 
     saved_posts = []
     saved_comments = []
+    saved_links = []
 
     for post in saved:  # separate out posts and comments
         if isinstance(post, praw.models.Submission):
-            saved_posts.append(post)
+            if 'reddit' in post.url:
+                saved_posts.append(post)
+            else:
+                saved_links.append(post)
         elif isinstance(post, praw.models.Comment):
             saved_comments.append(post)
 
-    saved_posts_title = []
+    saved_posts_title = {}
+    saved_comments_body = {}
+    saved_links_title = {}
 
     for post in saved_posts:    # create a list of saved posts' titles
-        saved_posts_title.append(post.title)
-        # print(post.title)
+        saved_posts_title[post.id] = post.title
+
+    for comment in saved_comments:  # create a list of saved comment's body
+        saved_comments_body[comment.id] = comment.body
+
+    for post in saved_links:    # create a list of saved links' titles
+        saved_links_title[post.id] = post.title
+
     while(True):
-        text_to_search = input('\nEnter the text to search:')
-        search(text=text_to_search, saved_posts=saved_posts_title)
-        url = print('\n[Post Number] to open the link in your default web browser')
+        choice = int(input(
+            '\nSearch for [1] saved posts, [2] saved comments, [3] saved link posts: '))
+        text_to_search = input('\nEnter the text to search: ')
+
+        if choice == 1:
+            search_results = search(
+                text=text_to_search, saved_posts=saved_posts_title)
+        elif choice == 2:
+            search_results = search(
+                text=text_to_search, saved_posts=saved_comments_body)
+        elif choice == 3:
+            search_results = search(
+                text=text_to_search, saved_posts=saved_links_title)
+        else:
+            print('\nPlease enter a valid choice.')
+            continue
+
+        post_number = int(input(
+            '\nEnter the post number[0 to return to search] to open in your default browser: '))
+        if post_number > len(search_results):
+            print('\nPlease enter a valid choice.')
+            continue
+
+        if post_number == 0:
+            continue
+        else:
+            if choice == 1:
+                for post in saved_posts:
+                    if post.id == search_results[post_number]:
+                        open_in_browser(post.url)
+                        break
+            elif choice == 2:
+                for post in saved_comments:
+                    if post.id == search_results[post_number]:
+                        open_in_browser(post.link_url + post.id)
+                        break
+            elif choice == 3:
+                for post in saved_links:
+                    if post.id == search_results[post_number]:
+                        link_choice = int(
+                            input('\n[1] to open link in browser, [2] to open reddit post in browser: '))
+                        if link_choice == 1:
+                            open_in_browser(post.url)
+                            break
+                        elif link_choice == 2:
+                            open_in_browser(
+                                'https://www.reddit.com' + post.permalink)
+                            break
+                        else:
+                            print('\nPlease enter a valid choice.')
+                            break
 
 
 if __name__ == '__main__':
